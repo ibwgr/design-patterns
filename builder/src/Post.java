962ff936d99b6fb29986a3b067a0ba46628ab5dc
samu <sandro.muggli@finnova.com>
2020-11-17 17:13:09 +0100

    solution for ex4_builder

--- builder/src/Post.java
+++ builder/src/Post.java
@@ -4,22 +4,42 @@ public class Post {
     private String datePosted;
     private int numberOfWords;
     private int numberOfCharacters;
-    public Post(String title, String author) {
-        this(title, author, null);
+
+    private Post(Builder builder) {
+        this.title = builder.title;
+        this.author = builder.author;
+        this.datePosted = builder.datePosted;
+        this.numberOfWords = builder.numberOfWords;
+        this.numberOfCharacters = builder.numberOfCharacters;
     }
-    public Post(String title, String author, String datePosted) {
-        this(title, author, datePosted, 0);
+
+    public static class Builder {
+        private String title;
+        private String author;
+        private String datePosted;
+        private int numberOfWords;
+        private int numberOfCharacters;
+        public Builder(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+        public Builder datePosted(String datePosted) {
+            this.datePosted = datePosted;
+            return this;
+        }
+        public Builder numberOfWords(int numberOfWords) {
+            this.numberOfWords = numberOfWords;
+            return this;
+        }
+        public Builder numberOfCharacters(int numberOfCharacters) {
+            this.numberOfCharacters = numberOfCharacters;
+            return this;
+        }
+        public Post build() {
+            return new Post(this);
+        }
     }
-    public Post(String title, String author, String datePosted, int numberOfWords) {
-        this(title, author, datePosted, numberOfWords, 0);
-    }
-    public Post(String title, String author, String datePosted, int numberOfWords, int numberOfCharacters) {
-        this.title = title;
-        this.author = author;
-        this.datePosted = datePosted;
-        this.numberOfWords = numberOfWords;
-        this.numberOfCharacters = numberOfCharacters;
-    }
+
 
     @Override
     public String toString() {
