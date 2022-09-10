268eb85bfd8143452f8e8be6436c6bc589c4e562
Marco Rensch <marco.rensch@nx-designs.ch>
2022-09-10 13:49:40 +0200

    Lektion 4 Aufgabe 1

--- proxy/src/second/ProxyPicture.java
+++ proxy/src/second/ProxyPicture.java
@@ -1,13 +1,29 @@
 package second;
 
-public class ProxyPicture {
+public class ProxyPicture implements Picture {
     private String fileName;
+    private String cachedPicture;
 
     public ProxyPicture(String fileName) {
         this.fileName = fileName;
     }
 
     public void draw() {
-        new RealPicture(fileName).display();
+        this.display();
+    }
+
+    @Override
+    public void display() {
+        if(cachedPicture != null && !cachedPicture.isEmpty()){
+            cached();
+        }else{
+            System.out.println("Displaying placeholder " + fileName);
+            new RealPicture(fileName).display();
+            cachedPicture = "cached.png";
+        }
+    }
+
+    public void cached(){
+        System.out.println("Displaying cached " + fileName);
     }
 }
